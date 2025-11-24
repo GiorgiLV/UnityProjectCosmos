807b851559d5bda1c64c24df368dfcbe0f6389ea
LoRd-KeKsiK <elisejgordienko3@gmail.com>
2025-11-24 17:54:04 +0300

    jostickAdd

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -5,14 +5,11 @@ public class CameraMove : MonoBehaviour
     public GameObject target;
     public float speed;
     private Vector3 targetPos;
-    void Start()
-    {
-        targetPos = target.transform.position;
-        targetPos.z = -10;
-    }
     
     void Update()
     {
-        gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, targetPos, speed * Time.deltaTime);
+        targetPos = target.transform.position;
+        targetPos.z = -10;
+        gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, targetPos, speed * Time.deltaTime);
     }
 }
