ba976fc10c12705a34696283a42d048ada2e17b0
LoRd-KeKsiK <elisejgordienko3@gmail.com>
2025-11-24 18:11:07 +0300

    commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float X, Y;
+    public float Speed;
+    public Joystick joystick;
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        X = joystick.Horizontal;
+        Y = joystick.Vertical;
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new  Vector2(X * Speed, Y * Speed);
+    }
+}
